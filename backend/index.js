612b6c27d1867b0c1cb39612bfb2401528b85426
Pavel Kholkin <xpaul6@gmail.com>
2025-09-28 19:23:18 +0500

    b:feat: system information route added

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import swaggerUi from 'swagger-ui-express'
 import swaggerJsDoc from 'swagger-jsdoc'
 
+import si from 'systeminformation'
+
 const app = express()
 const PORT = 8080
 
@@ -35,7 +37,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
  *       200:
  *         description: A successful response.
  */
-app.get('/server-status', (_, res) => { res.send('Server is up!') })
+app.get('/server-status', (_, res) => { return res.send('Server is up!') })
 
 /**
  * @swagger
@@ -74,10 +76,60 @@ app.get('/mcstatus/:address', async (req, res) => {
   try {
     const apiResponse = await fetch(`https://api.mcstatus.io/v2/status/java/${req.params.address}`);
     const info = await apiResponse.json();
-    res.status(200).send(info);
+    return res.status(200).send(info);
   } catch (err) {
     console.log(err);
-    res.status(500).send({ error: 'Failed to fetch Minecraft server status' });
+    return res.status(500).send({ error: 'Failed to fetch Minecraft server status' });
+  }
+})
+
+/**
+ * @swagger
+ * /sysinfo:
+ *   get:
+ *     summary: Gets system information.
+ *     description: Returns basic system information like CPU temperature, CPU load, and RAM usage.
+ *     responses:
+ *       200:
+ *         description: Successful response containing the system information.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 cpu_temp:
+ *                   type: number
+ *                   description: "CPU temperature in Celsius."
+ *                   example: 45.5
+ *                 cpu_load:
+ *                   type: number
+ *                   description: "CPU load in percent."
+ *                   example: 15.2
+ *                 ram_percent:
+ *                   type: number
+ *                   description: "RAM usage as a percentage."
+ *                   example: 0.65
+ *       500:
+ *         description: Error fetching the system information.
+ */
+app.get('/sysinfo', async (_, res) => {
+  try {
+    const [temp, load, mem] = await Promise.all([
+      si.cpuTemperature(),
+      si.currentLoad(),
+      si.mem()
+    ]);
+
+    const result = {
+      cpu_temp: temp.main,
+      cpu_load_percent: load.currentLoad,
+      ram_percent: mem.active / mem.total * 100
+    };
+
+    return res.status(200).send(result);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ error: 'Failed to fetch system information' });
   }
 })
 
