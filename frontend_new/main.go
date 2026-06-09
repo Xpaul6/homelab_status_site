5142eb5c8a1a6f2919136c58a3b16ed2139b0947
Pavel Kholkin <xpaul6@gmail.com>
2026-06-10 02:34:32 +0500

    f:fix: correct backend requests rerouting for containers

--- frontend_new/main.go
+++ frontend_new/main.go
@@ -1,10 +1,33 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./")))
-	http.ListenAndServe(":5555", nil)
+	target, _ := url.Parse("http://dash-backend:8080")
+
+	proxy := &httputil.ReverseProxy{
+		Rewrite: func(pr *httputil.ProxyRequest) {
+			pr.SetURL(target)
+
+			path := strings.TrimPrefix(pr.In.URL.Path, "/api")
+			if path == "" {
+				path = "/"
+			}
+
+			pr.Out.URL.Path = path
+			pr.Out.Host = pr.Out.URL.Host
+		},
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/api/", proxy)
+	mux.Handle("/", http.FileServer(http.Dir("./")))
+
+	log.Fatal(http.ListenAndServe(":5555", mux))
 }
